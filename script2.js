f65a0cb7c95a33562199c0d4713781010fca3117
Lucas Gonçalves <lucasgnclvs1@gmail.com>
2024-09-03 20:42:16 -0300

    feat: add script to solve exercise three
    code developed to solve exercise three showing the result of the proposed code

---
+++ script2.js
@@ -0,0 +1,15 @@
+function runExercise3() {
+
+    let idx = 12;
+    let sum = 0;
+    let K = 1;
+
+    while (K < idx) {
+        K = K + 1;
+        sum += K;
+    }
+
+    alert(`O valor da variavel soma é: ${sum}.`);
+}
+
+runExercise3();
